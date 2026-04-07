c76130feb461d2628a7acc4c42a9271ac6581484
Eva Alonso <evam.alonso95@gmail.com>
2026-04-07 10:21:42 +0200

    feat: add EmailService for sending emails and update environment configuration

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,8 @@
-import { CheckService } from "../domain/use-cases/checks/check-service";
+// import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImp } from "../infrastructure/repositories/log.repository.impl";
-import { CronService } from "./cron/cron-service";
+// import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 //Instancias para todos los useCase de ese repositorio
 const fileSystemLogRepository = new LogRepositoryImp(
@@ -14,7 +15,12 @@ export class Server {
   public static start() {
     console.log("Server started...");
 
-    //Mandar email
+    //todo: Mandar email
+    // const emailService = new EmailService(fileSystemLogRepository);
+    // emailService.sendEmailWithFileSystemLogs([
+    //   "evaalonso888@gmail.com",
+    //   "evam.alonso95@gmail.com",
+    // ]);
 
     // const url = "https://google.com";
     // CronService.createJob("*/5 * * * * *", () => {
@@ -28,5 +34,3 @@ export class Server {
     // });
   }
 }
-
-// Server.start();
