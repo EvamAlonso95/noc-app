d1d0d1f300744ea1566483fb37ffdb81f4c3fe18
Eva Alonso <evam.alonso95@gmail.com>
2026-04-09 11:58:12 +0200

    feat: update coverage reports and tests for envs.plugin.ts with new metrics and error handling

--- src/config/plugins/envs.plugin.test.ts
+++ src/config/plugins/envs.plugin.test.ts
@@ -3,6 +3,29 @@ import { envs } from "./envs.plugin";
 
 describe("Test envs.plugin.ts", () => {
   test("should return env options", () => {
-    console.log(envs);
+    expect(envs).toEqual({
+      PORT: 3000,
+      MAILER_SERVICE: "gmail",
+      MAILER_EMAIL: "evaalonso888@gmail.com",
+      MAILER_SECRET_KEY: "tigbsxkoknvwjrdn",
+      PROD: false,
+      MONGO_URL: "mongodb://eva:123456@127.0.0.1:27017",
+      MONGO_DB_NAME: "NOC-TEST",
+      MONGO_USER: undefined,
+      MONGO_PASS: "123456",
+      POSTGRES_URL: "postgresql://postgres:123456@localhost:5432/NOC",
+    });
+  });
+
+  test("should return error if not found env", async () => {
+    jest.resetModules();
+    process.env.PORT = "ABC";
+
+    try {
+      await import("./envs.plugin");
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(`${error}`).toContain('"PORT" should be a valid integer');
+    }
   });
 });
